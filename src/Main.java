bf0391bf3eda002c0ffdcb4f603d0d4d23a16d9c
Yuki Dimov <MSDikmeshefket19@codingburgas.bg>
2022-10-19 13:35:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Lamp lamp = new Lamp();
+        lamp.SetName("Lampa");
+        System.out.println(lamp.GetName());
+    }
+
+
+}
